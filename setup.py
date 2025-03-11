3edf1500e4f6226784ef77160f0b234d5efa4148
Alexey Gorbatovski <porshJob@yandex.ru>
2025-03-11 14:15:08 +0300

    commit

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ _deps = [
     "langdetect",  # Needed for LightEval's extended tasks
     "latex2sympy2_extended>=1.0.6",
     "liger_kernel==0.5.3",
-    "lighteval @ git+https://github.com/myashka/lighteval.git@2848c6313a700812b28a06db5a74f720413dc97b",
+    "lighteval @ git+https://github.com/myashka/lighteval.git@afcb85f3fbb64096aecac72f3a2f6aa416542268",
     "math-verify==0.5.2",  # Used for math verification in grpo
     "packaging>=23.0",
     "parameterized>=0.9.0",
