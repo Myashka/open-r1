e929f61d1e51d806502dd05d3be7e3ea6f45b258
Alexey Gorbatovski <porshJob@yandex.ru>
2025-03-15 20:01:00 +0300

    Fix

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ _deps = [
     "langdetect",  # Needed for LightEval's extended tasks
     "latex2sympy2_extended>=1.0.6",
     "liger_kernel==0.5.3",
-    "lighteval @ git+https://github.com/myashka/lighteval.git@a3ffc6a337e91403aaa68c653e345739ac93dbb8",
+    "lighteval @ git+https://github.com/myashka/lighteval.git@08f9d64ad7ec37f9cfeb910f15a2022458b7c4f5",
     "math-verify==0.5.2",  # Used for math verification in grpo
     "packaging>=23.0",
     "parameterized>=0.9.0",
