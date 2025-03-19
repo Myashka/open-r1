67ff237273595552e6367acc668ef824d6fd18c7
Alexey Gorbatovski <porshJob@yandex.ru>
2025-03-19 12:43:49 +0300

    Fix

--- src/open_r1/evaluate_limo.py
+++ src/open_r1/evaluate_limo.py
@@ -1,17 +1,3 @@
-# Copyright 2025 The HuggingFace Team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 """Custom evaluation tasks for LightEval."""
 
 import random
@@ -34,7 +20,7 @@ B) {B}
 C) {C}
 D) {D}
 
-Please reason step-by-step and put your choice letter without any other text with \\boxed{} in the end.
+Please reason step-by-step and put your choice letter without any other text with \\boxed{{}} in the end.
 """.strip()
 
 latex_gold_metric = multilingual_extractive_match_metric(
